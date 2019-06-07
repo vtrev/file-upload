85fb9311ce1b89684e54362413fd122426129078
vtrev <baloyi.trev@gmail.com>
2019-06-07 16:43:18 +0200

    changed resizing lib from sharp to image-js

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const fileUpload = require('express-fileupload');
 const sharp = require('sharp');
 const app = express();
 const image2base64 = require('image-to-base64');
+const imageInfo = require('image-info');
+const {
+	Image
+} = require('image-js');
+
+
 
 app.use(express.static("./client/dist"));
 app.use(fileUpload());
@@ -21,33 +27,56 @@ app.post('/api/uploadQuizAttachment', function (req, res) {
 		return res.status(400).send('invalid image type');
 	}
 
+	//methods
+
 	let generateFileName = (params) => {
 		return (params.studentName + "-" + params.quizId + ".png").trim();
 	}
 
-	let fileName = generateFileName({
-		studentName: 'vusi',
-		quizId: '001'
-	});
-
-	sharp(quizAttachment.data).resize({
-			width: 1024
-		})
-		.toFile(`./quizattachments/${fileName}`, (err, info) => {
-			if (!err) {
-				image2base64(`./quizattachments/${fileName}`)
-					.then(
-						(response) => {
-						res.send({base64img:response}); 						}
-					)
-					.catch(
-						(error) => {
-							console.log(error);
-						}
-					)
-			} else
+	let sendBase64res = (imagePath)=>{
+		image2base64(imagePath).then(
+			(response) => {
+				res.send({
+					base64img: response
+				});
+			})
+		.catch(
+			(error) => {
 				res.send('Error while trying to save file');
+				console.log(error);
+			})
+	}
+
+	let resizeImageToFile = async (image, fileName) => {
+
+		let imageFile = await Image.load(image);
+		let tmpImage = imageFile;
+
+		if (imageFile.width > 1024) {
+			tmpImage = imageFile.resize({
+				width: 1024
+			});
+		}
+		if (imageFile.height > 1024) {
+			tmpImage = tmpImage.resize({
+				height: 1024
+			})
+		}
+
+		await tmpImage.save(`./quizattachments/${fileName}`);
+		
+		return `./quizattachments/${fileName}`
+	}
+	
+	let fileName = generateFileName({
+			studentName: 'vusi',
+			quizId: '001'
 		});
+
+	resizeImageToFile(quizAttachment.data,fileName).then((res) =>{
+		sendBase64res(res);
+	})
+
 });
 
 const PORT = process.env.PORT || 5656;
